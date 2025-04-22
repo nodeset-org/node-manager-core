22095e9ac7134ce03fb8b2736da1e3e04d4fdc7d
Joe Clapis <jclapis@outlook.com>
2025-04-21 22:52:03 -0400

    Updated Lighthouse

--- config/lighthouse-bn-config.go
+++ config/lighthouse-bn-config.go
@@ -6,8 +6,8 @@ import (
 
 const (
 	// Tags
-	lighthouseProdBnTag string = "sigp/lighthouse:v6.0.1"
-	lighthouseTestBnTag string = "sigp/lighthouse:v7.0.0-beta.7"
+	lighthouseProdBnTag string = "sigp/lighthouse:v7.0.0"
+	lighthouseTestBnTag string = "sigp/lighthouse:v7.0.0"
 )
 
 // Configuration for the Lighthouse BN
